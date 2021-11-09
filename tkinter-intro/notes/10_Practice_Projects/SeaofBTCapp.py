992c087fd468b9ffe0584af7b96e2263ba6d9d81
zhd204@gmail.com <zhd204@gmail.com>
2021-11-08 22:18:31 -0500

    update

--- tkinter-intro/notes/10_Practice_Projects/SeaofBTCapp.py
+++ tkinter-intro/notes/10_Practice_Projects/SeaofBTCapp.py
@@ -2,6 +2,7 @@
 # https://pythonprogramming.net/tkinter-depth-tutorial-making-actual-program/
 
 import tkinter as tk
+from tkinter import ttk
 
 LARGE_FONT = ("Verdana", 16)
 
@@ -9,11 +10,11 @@ LARGE_FONT = ("Verdana", 16)
 class SeaofBTCapp(tk.Tk):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.iconbitmap("clienticon.ico")
+        self.iconbitmap("clienticon.ico")  # for windows
         self.title("Sea of BTC Client")
         self.attributes("-topmost", 1)
 
-        container = tk.Frame(self)
+        container = ttk.Frame(self)
         container.pack(side="top", fill="both", expand=True)
         container.grid_rowconfigure(0, weight=1)
         container.grid_columnconfigure(0, weight=1)
@@ -31,48 +32,48 @@ class SeaofBTCapp(tk.Tk):
         frame.tkraise()
 
 
-class StartPage(tk.Frame):
+class StartPage(ttk.Frame):
     def __init__(self, parent, controller):
         super().__init__(parent)
-        label = tk.Label(self, text="Start Page", font=LARGE_FONT)
+        label = ttk.Label(self, text="Start Page", font=LARGE_FONT)
         label.pack(padx=10, pady=10)
 
-        button1 = tk.Button(self, text="Visit Page 1",
-                            command=lambda: controller.show_frame(PageOne))
+        button1 = ttk.Button(self, text="Visit Page 1",
+                             command=lambda: controller.show_frame(PageOne))
         button1.pack()
 
-        button2 = tk.Button(self, text="Visit Page 2",
-                            command=lambda: controller.show_frame(PageTwo))
+        button2 = ttk.Button(self, text="Visit Page 2",
+                             command=lambda: controller.show_frame(PageTwo))
         button2.pack()
 
 
-class PageOne(tk.Frame):
+class PageOne(ttk.Frame):
     def __init__(self, parent, controller):
         super().__init__(parent)
-        label = tk.Label(self, text="Page One", font=LARGE_FONT)
+        label = ttk.Label(self, text="Page One", font=LARGE_FONT)
         label.pack(padx=10, pady=10)
 
-        button = tk.Button(self, text="Back to Home",
-                           command=lambda: controller.show_frame(StartPage))
+        button = ttk.Button(self, text="Back to Home",
+                            command=lambda: controller.show_frame(StartPage))
         button.pack()
 
-        button2 = tk.Button(self, text="Visit Page 2",
-                            command=lambda: controller.show_frame(PageTwo))
+        button2 = ttk.Button(self, text="Visit Page 2",
+                             command=lambda: controller.show_frame(PageTwo))
         button2.pack()
 
 
-class PageTwo(tk.Frame):
+class PageTwo(ttk.Frame):
     def __init__(self, parent, controller):
         super().__init__(parent)
-        label = tk.Label(self, text="Page Two", font=LARGE_FONT)
+        label = ttk.Label(self, text="Page Two", font=LARGE_FONT)
         label.pack(padx=10, pady=10)
 
-        button = tk.Button(self, text="Back to Home",
-                           command=lambda: controller.show_frame(StartPage))
+        button = ttk.Button(self, text="Back to Home",
+                            command=lambda: controller.show_frame(StartPage))
         button.pack()
 
-        button1 = tk.Button(self, text="Visit Page 1",
-                            command=lambda: controller.show_frame(PageOne))
+        button1 = ttk.Button(self, text="Visit Page 1",
+                             command=lambda: controller.show_frame(PageOne))
         button1.pack()
 
 
